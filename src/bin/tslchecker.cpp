7866be0881098d18d120cac48750eccf9af6b07f
Tyler Schmidt <tmschmid@protonmail.com>
2024-08-14 16:29:56 -0700

    Add executable tslchecker.

---
+++ src/bin/tslchecker.cpp
@@ -0,0 +1,20 @@
+#include <filesystem>
+#include <iostream>
+
+#include "tsl_parser.hpp"
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Invalid Number of arguments. Expected 2, found " << argc << std::endl;
+        return 1;
+    }
+
+    std::filesystem::path inputFilePath(argv[argc - 1]);
+    if (!std::filesystem::exists(inputFilePath)) {
+        std::cerr << "File " << inputFilePath << " does not exist." << std::endl;
+        return 1;
+    }
+
+    TSLParser parser(inputFilePath);
+    return parser.run();
+}
