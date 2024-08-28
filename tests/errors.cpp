4129872fcec9d55ad70343a536fa2eedd2683df5
Tyler Schmidt <tmschmid@protonmail.com>
2024-08-28 15:10:27 -0700

    Add new error scenario detecting errors later in file.

--- tests/errors.cpp
+++ tests/errors.cpp
@@ -54,11 +54,37 @@ SCENARIO("The Parser reports a malformed Category.") {
         WHEN("error messages are redirected to be read by us,") {
             auto stderrListener = ErrorMessageListener();
 
-            WHEN("the Compiler consumes the input,") {
+            WHEN("the Parser consumes the input,") {
                 TSLParser parser(tslInput);
                 parser.run();
-                THEN("an error message should be displayed relating to an invalid Category.") {
+                THEN("the error message should point to line 1.") {
+                    REQUIRE(stderrListener.getCurrentErrorMsg().find("tests/invalid_category.txt:1") != std::string::npos);
+                }
+
+                THEN("the error message should mention expecting a Category.") {
                     REQUIRE(stderrListener.getCurrentErrorMsg().find("CATEGORY_CONTENTS") != std::string::npos);
+                }
+            }
+        }
+    }
+}
+
+SCENARIO("The Parser reports an invalid Choice.") {
+    GIVEN("a TSL input with a Category, and a Choice that ends with a '?' instead of a '.',") {
+        fs::path tslInput = "tests/invalid_choice.txt";
+        WHEN("error messages are redirected to be read by us,") {
+            auto stderrListener = ErrorMessageListener();
+
+            WHEN("the Parser consumes the input,") {
+                TSLParser parser(tslInput);
+                parser.run();
+                THEN("the error message should point to line 2.") {
+                    INFO(stderrListener.getCurrentErrorMsg());
+                    REQUIRE(stderrListener.getCurrentErrorMsg().find("tests/invalid_choice.txt:2") != std::string::npos);
+                }
+
+                THEN("the error message should mention expecting a Choice.") {
+                    REQUIRE(stderrListener.getCurrentErrorMsg().find("CHOICE_CONTENTS") != std::string::npos);
                 }
             }
         }
--- tests/feature-files/errors.feature
+++ tests/feature-files/errors.feature
@@ -1,6 +1,14 @@
 Feature: An error message should be provided when an issue comes up with the Lexer or Parser.
-    Scenario: The Lexer reports an unrecognized symbol.
+    Scenario: The Parser reports an unrecognized symbol.
         Given a TSL input with a Category that ends with a '?' instead of a ':',
         When error messages are redirected to be read by us,
-        And the Compiler consumes the input,
-        Then an error message should be displayed relating to an invalid Category.
+        And the Parser consumes the input,
+        Then the error message should point to line 1.
+        And the error message should mention expecting a Category.
+
+    Scenario: The Parser reports an invalid Choice.
+        Given a TSL input with a Category, and a Choice that ends with a '?' instead of a '.',
+        When the error messages are redirected to be read by us,
+        And the Parser consumes the input,
+        Then the error message should point to line 2.
+        And the error message should mention expecting a Choice.
